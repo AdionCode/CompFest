70151ef40b6e90a6c9adb130007c59540d44d90b
Hans <reyhan.supangkat01@gmail.com>
2022-07-02 18:53:59 +0700

    CommitAwal
    Mindah file dari TestField

---
+++ CursedLand/Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInputAction playerInput;
+    private Rigidbody2D rb;
+    private Vector3 position;
+    private Vector3 velocity;
+
+    private float horizontal;
+    [SerializeField] private float speed = 5f;
+    private bool isFacingRight = false;
+    
+    void Awake()
+    {
+        playerInput = new PlayerInputAction();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    
+    void FixedUpdate()
+    {
+        Vector2 moveInput = playerInput.Player.Move.ReadValue<Vector2>();
+
+        horizontal = moveInput.x;
+
+        rb.velocity = moveInput * speed;
+
+        if (!isFacingRight && horizontal > 0f)
+        {
+            flip();
+        }
+        else if(isFacingRight && horizontal < 0f)
+        {
+            flip();
+        }
+    }
+
+    private void flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+}
