58d7cf9943767ee0619b4ac6f69a06842e4da550
Hans <reyhan.supangkat01@gmail.com>
2022-07-12 20:56:20 +0700

    mindah ke folder sprite

---
+++ CursedLand/Assets/Goblin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour
+{
+    public float speed = 3f;
+    private Transform target;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
