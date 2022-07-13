0c6a7e2ea521ed38474811889b0c2d960a1241fe
Hans <reyhan.supangkat01@gmail.com>
2022-07-13 10:11:22 +0700

    add goblin follow

--- CursedLand/Assets/Goblin.cs
+++ CursedLand/Assets/Goblin.cs
@@ -6,10 +6,31 @@ public class Goblin : MonoBehaviour
 {
     public float speed = 3f;
     private Transform target;
-   
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.gameObject.tag == "Player")
+        {
+            target = other.transform;
+            Debug.Log("Masuk");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            target = null;
+            Debug.Log("Keluar");
+        }
+    }
+    private void Update()
+    {
+        if(target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
         
     }
 }
