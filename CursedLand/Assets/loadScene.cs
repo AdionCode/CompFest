dfd1789597c509f9eccdb72bc229bd4e5322092c
Rifqi Adi Mu'Ammar <2011016210018@mhs.ulm.ac.id>
2022-08-12 17:53:38 +0800

    Menu Scene
    Basic Menu UI Play & Exit

---
+++ CursedLand/Assets/loadScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+    
+}
