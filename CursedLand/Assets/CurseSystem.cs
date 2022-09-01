24000595933e6981baa8dc3c0d2788c36c38c9cd
Hans <reyhan.supangkat01@gmail.com>
2022-09-01 22:56:44 +0800

    C

--- CursedLand/Assets/CurseSystem.cs
+++ CursedLand/Assets/CurseSystem.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class CurseSystem : MonoBehaviour
 {
     // Start is called before the first frame update
+    private static CurseSystem instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one Curse System in the scene");
+        }
+        instance = this;
+    }
+
+    public static CurseSystem GetInstance()
+    {
+        return instance;
+    }
+
     void Start()
     {
         
